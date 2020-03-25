ef760c862948c1bc1000ed9f72d56432b472be16
amenhotep74 <Blewie144@gmail.com>
2020-03-25 15:07:08 +1030

    complete

--- index.js
+++ index.js
@@ -1,4 +1,31 @@
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('Hello World');
+var http = require('http');
+var url = require('url');
+var fs = require('fs');
+
+http.createServer(function(req, res) {
+    // Break up the url requested from the search 
+    const q = url.parse(req.url, true);
+    let fileread;
+    if (q.pathname === '/') {
+        fileread = './index.html';
+    } else {
+        fileread = '.' + q.pathname + '.html';
+    }
+    fs.readFile(fileread, function(err, data) {
+        if (err) {
+            // if there is an error display the error page 
+            const errorPath = './404.html';
+            fs.readFile(errorPath, function(err, data) {
+                res.writeHead(404, { 'Content-Type': 'text/html' });
+                res.write(data);
+                return res.end();
+            });
+        } else {
+            // if no errors display the fileread 
+            res.writeHead(200, { 'Content-Type' : 'text/html' });
+            res.write(data);
+            return res.end();
+        }
+    });
+
 }).listen(8080);
