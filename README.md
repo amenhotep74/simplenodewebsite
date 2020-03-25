7c20eb3f56cd01cf9f7674889701fcd5293cf59b
amenhotep74 <Blewie144@gmail.com>
2020-03-25 17:50:58 +1030

    refactored using express

---
+++ README.md
@@ -0,0 +1,2 @@
+Simple file server using nodeJS 
+the url name is captured and then a file is search in the root folder with that name, then that page is displayed. 
--- index.js
+++ index.js
@@ -1,31 +1,30 @@
+const express = require('express');
+const app = express();
+
 var http = require('http');
 var url = require('url');
 var fs = require('fs');
 
-http.createServer(function(req, res) {
-    // Break up the url requested from the search 
-    const q = url.parse(req.url, true);
-    let fileread;
-    if (q.pathname === '/') {
-        fileread = './index.html';
-    } else {
-        fileread = '.' + q.pathname + '.html';
-    }
-    fs.readFile(fileread, function(err, data) {
-        if (err) {
-            // if there is an error display the error page 
-            const errorPath = './404.html';
-            fs.readFile(errorPath, function(err, data) {
-                res.writeHead(404, { 'Content-Type': 'text/html' });
-                res.write(data);
-                return res.end();
-            });
-        } else {
-            // if no errors display the fileread 
-            res.writeHead(200, { 'Content-Type' : 'text/html' });
-            res.write(data);
-            return res.end();
-        }
-    });
+// Refactored using express 
+var path = require('path');
 
-}).listen(8080);
+if(err) {
+    res.send('Error 404');
+}
+
+// viewed at http://localhost:8080 
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.get('/about', function(req, res) {
+    res.sendFile(path.join(__dirname + '/about.html'));
+});
+
+app.get('/contact-me', function(req, res) {
+    res.sendFile(path.join(__dirname + '/contact-me.html'));
+});
+
+
+
+app.listen(8080);
