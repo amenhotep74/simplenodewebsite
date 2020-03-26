7265328516a0e4a4dedbd3e621cf24d5b3910636
amenhotep74 <Blewie144@gmail.com>
2020-03-26 13:22:58 +1030

    complete

--- index.js
+++ index.js
@@ -1,28 +1,11 @@
 const express = require('express');
+const path = require('path');
+
 const app = express();
 
-var http = require('http');
-var url = require('url');
-var fs = require('fs');
+// Set a static folder 
+app.use(express.static(path.join(__dirname, 'public')));
 
-// Refactored using express 
-var path = require('path');
+const PORT = process.env.PORT || 8080;
 
-if(err) {
-    res.send('Error 404');
-}
-
-// viewed at http://localhost:8080 
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/index.html'));
-});
-
-app.get('/about', function(req, res) {
-    res.sendFile(path.join(__dirname + '/about.html'));
-});
-
-app.get('/contact-me', function(req, res) {
-    res.sendFile(path.join(__dirname + '/contact-me.html'));
-});
-
-app.listen(8080);
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
